69d70cc4f7620ceb79078f1dc0090e54b2463512
nickpwells <wells.nick53@gmail.com>
2015-05-03 13:38:59 -0700

    added ryu cool

--- js/app.js
+++ js/app.js
@@ -26,6 +26,18 @@ $(document).ready(function(){
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   	});
+  
+  $(this).keydown(function(event){
+      if (event.which == 88){
+        $('.ryu-still').hide();
+        $('.ryu-cool').show();
+      };
+    })
+    .keyup(function(){
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    });
+  
   function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
